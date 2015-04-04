63b80a8da929a6729a6dd261746d8774a6e3db39
Owen <cuizhefc@berkeley.edu>
2015-04-03 18:43:48 -0700

    user login feature done

--- features/step_definitions/pending_steps.rb
+++ features/step_definitions/pending_steps.rb
@@ -50,7 +50,8 @@ When(/^I log in as a student$/) do
   pending # express the regexp above with the code you wish you had
 end
 Given(/^I am on the Staging Page$/) do
-  pending # express the regexp above with the code you wish you had
+  visit new_user_session_path
+  # pending # express the regexp above with the code you wish you had
 end
 When(/^I press the "(.*?)" button$/) do |arg1|
   pending # express the regexp above with the code you wish you had
