998e9aeadb06314b0a1b9a2f1c2180df7e9dea40
lenazh <lena.zhivun@berkeley.edu>
2015-04-10 01:08:46 -0300

    Abstract class is up, but not passing tests. See spec/models/abstract_question_spec.rb . Also migrate.

--- app/models/questions/abstract_question.rb
+++ app/models/questions/abstract_question.rb
@@ -1,6 +1,6 @@
 # == Schema Information
 #
-# Table name: other_questions
+# Table name: questions_v2
 #
 #  id         :integer          not null, primary key
 #  content    :text
@@ -13,17 +13,21 @@
 
 class AbstractQuestion < ActiveRecord::Base
 
+  attr_accessor :content
+
   has_many :relationships, dependent: :destroy
   has_many :quizzes, through: :relationships, foreign_key: 'quiz_id'
 
   serialize :options, JSON
 
+  self.table_name = "questions_v2"
+
 # some metaprogramming for easy attribute access?
-  def method_missing(method_sym, argument)
+  def method_missing(method_sym, *arguments, &block)
     if method_sym.to_s =~ /^opt_(.*)=$/
-      options[$1.to_sym] = argument
+      self.options[$1.to_sym] = arguments.first
     elsif method_sym.to_s =~ /^opt_(.*)$/
-      options[$1.to_sym]
+      self.options[$1.to_sym]
     else
       super
     end
@@ -52,6 +56,10 @@ class AbstractQuestion < ActiveRecord::Base
 # returns the credit given for the answer
   def autograde(answer)
   	return 0
+  end
+
+  def grade(answer)
+    autograde(answer)
   end
 
   def partial
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150329231806) do
+ActiveRecord::Schema.define(version: 20150410033820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 20150329231806) do
     t.datetime "updated_at"
     t.string   "lesson",     default: ""
     t.string   "difficulty"
+  end
+
+  create_table "questions_v2", force: true do |t|
+    t.text     "content"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.string   "lesson",     default: ""
+    t.string   "difficulty"
+    t.text     "options"
   end
 
   create_table "quiz_locks", force: true do |t|
