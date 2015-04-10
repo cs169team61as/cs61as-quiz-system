e57d156fb8ffcabeaddfe1e17fc770671b9d14aa
lenazh <lena.zhivun@berkeley.edu>
2015-04-09 23:59:19 -0300

    Abstract question class skeleton

---
+++ spec/models/different_question_types_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "Simple class inherited from abstract question class"
+	it "should have a credit given for its completion"
+	it "should have a question text"
+	it "should be able to store the metadata in the database and retrieve it back"
+	it "should have the autograde() function"
+	it "should have a function returning the path to its partial"
+end
