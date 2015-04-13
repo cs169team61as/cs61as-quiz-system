d2647dfe230584c9169792f0f6759dfdd658b9ad
lenazh <lena.zhivun@berkeley.edu>
2015-04-13 16:56:22 -0300

    Populated empty Rubric/Solution objects

--- app/controllers/concerns/create_question.rb
+++ app/controllers/concerns/create_question.rb
@@ -4,8 +4,6 @@ class CreateQuestion
     question = AbstractQuestion.new question_params
     solution = Solution.new question_params.delete :solution_attributes
     rubric = Rubric.new question_params.delete :rubric_attributes
-    solution.content = question.make_solution
-    rubric.rubric = question.make_rubric
 
     question.solution = solution
     question.rubric = rubric
