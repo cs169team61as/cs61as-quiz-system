5c55768baf80c207faa12e5023c4ec9f5586e370
lenazh <lena.zhivun@berkeley.edu>
2015-04-17 16:57:39 -0300

    Template for the ShortAnswerQuestion test and refactoring

--- app/models/questions/short_answer_question.rb
+++ app/models/questions/short_answer_question.rb
@@ -1,13 +1,29 @@
 class ShortAnswerQuestion < AbstractQuestion
   def autograde(answer, quiz_id)
-  	res = Hash.new
-  	if answer =~ /#{my_solution}/i
-    	res[:credit] = full_credit(quiz_id)
-    	res[:comment] = "The answer matches my solution (#{q.partial} autograder)"
-    else
-    	res[:credit] = 0
-    	res[:comment] = "The answer doesn't match my solution (#{q.partial} autograder)"
+  	if (my_solution == nil) || (my_solution == "")
+      return give_no_credit "I don't have a solution for this question"
     end
+
+  	if answer =~ /#{my_solution}/i
+    	return give_full_credit "The answer matches my solution", quiz_id
+    else
+    	return give_no_credit "The answer doesn't match my solution"
+    end
+  end
+
+  private
+
+  def give_full_credit(msg, quiz_id)
+    res = Hash.new
+    res[:credit] = full_credit(quiz_id)
+    res[:comment] = msg + "(#{q.partial} autograder)"
+    res
+  end
+
+  def give_no_credit(msg)
+    res = Hash.new
+    res[:credit] = 0
+    res[:comment] = msg + "(#{q.partial} autograder)"
     res
   end
 end
