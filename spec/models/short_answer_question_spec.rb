c87ae107c5540b10715382d97a700d00dd1af44b
lenazh <lena.zhivun@berkeley.edu>
2015-04-17 16:57:39 -0300

    Template for the ShortAnswerQuestion test and refactoring

---
+++ spec/models/short_answer_question_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe "ShortAnswerQuestion" do
+	it "should give full credit if the solution matches the answer, ignoring the case"
+	it "should give no credit if the solution doesn't match the answer"
+	it "should give no credit if the solution is not provided"
+end
