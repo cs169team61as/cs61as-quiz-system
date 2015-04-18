424d37a2ff7467baa9138ded881cbb93bd91eda2
lenazh <lena.zhivun@berkeley.edu>
2015-04-18 03:45:40 -0300

    Test for MultipleChoiceQuestion class

---
+++ spec/models/multiple_choice_question_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "MultipleChoiceQuestion" do
+	def grade(content)
+		question.autograde(content, quiz.id)[:credit]
+	end
+
+	let(:points) { 10 }
+
+	let!(:question) do 
+		q = MultipleChoiceQuestion.build :content => "Which of these numbers are even?"
+		q.choices = {"1" => false, "2" => true, "3" => false, "4" => true}
+		q.save
+		q
+	end
+  	let!(:quiz) do
+   		pq = build :quiz, retake: false, is_draft: false
+    	pq.save(:validate => false)
+    	pq.relationships.create!  question_id: question.id,
+                              number: 1,
+                              points: points
+    	pq
+  	end
+
+
+	it "should give full credit if all the correct boxes are checked and incorrect are not" do
+		expect(grade({"2" => nil, "4" => nil})).to eq points
+	end
+
+	it "should give partial credit if some of the correct answers are checked" do
+		expect(grade({"2" => nil})).to eq points/2
+		expect(grade({"1" => nil, "2" => nil, "4" => nil})).to eq points/2
+		expect(grade({"1" => nil, "2" => nil, "3" => nil, "4" => nil})).to eq 0
+	end
+
+	it "should not give less than zero credit" do
+		expect(grade({"1" => nil, "3" => nil})).to eq 0
+	end
+
+end
