382a933040be178fe7d3af977e0384cebee81cda
lenazh <lena.zhivun@berkeley.edu>
2015-04-18 03:01:55 -0300

    Multiple choice partial fixed

--- app/models/questions/multiple_choice_question.rb
+++ app/models/questions/multiple_choice_question.rb
@@ -7,7 +7,7 @@ all that apply. This will be defined by option single_answer which
 is true when there is only one choice needs to be selected to answer.
 
 :choices    is a hash with the options (string) and whether or
-            it should be checked.
+            it is a correct answer
 
             When single_answer is true the student only has to select
             one option. There can be one or more answers which are
@@ -18,18 +18,19 @@ is true when there is only one choice needs to be selected to answer.
             scores and correctness depending on the rubric.
 
 :single_answer  True when MCQ is a single answer question, false otherwise
+(maybe we should just inherit from this class, to update the 
+partials name automatically, like
+SimpleChoiceQuestion < MultipleChoiceQuestion
 
 :answers    Hash of all the potential answers and their respective scores
 
 Sample Question: "Which numbers are larger than 0 smaller than 2?"
 
-  :choices is ["0"=>false, "1"=>true, "2" =>true]
+  :choices is {"0"=>false, "1"=>true, "2" =>false}
   :single_answer is false
   :answers is [[false,true,true]=>2, [false,true,false]=>1
                 , [false,false,true]=>1, [true,true,true]=>1]
-  
- Simplified for this iteration:
-  :answers are ["0" => true, "2" => false, "4" => false] 
+
   Note: Answer representation up for discussion/change
 
 =end
@@ -37,9 +38,9 @@ Sample Question: "Which numbers are larger than 0 smaller than 2?"
   option_accessor :choices, :single_answer, :answers
 
   def autograde(content, quiz_id)
-    if (answers == nil)
-      return give_no_credit "I don't have a solution for this question"
-    end
+    # if (answers == nil)
+    #   return give_no_credit "I don't have a solution for this question"
+    # end
 
     @full_credit = full_credit(quiz_id)
     score = calculate_score(content)
@@ -50,20 +51,21 @@ Sample Question: "Which numbers are larger than 0 smaller than 2?"
       else
         return give_partial_credit(score, "The answer partially matches my solition")
       end
-    return give_no_credit "The answer doesn't match my solution"
+      return give_no_credit "The answer doesn't match my solution"
+    end
   end
 
   private
 
   def calculate_score(content)
     total_correct_answers = 0
-    answers.each do |key, value|
-      total_correct_answers += 1 if value
+    choices.each do |key, value|
+      total_correct_answers += 1 if value 
     end
 
     pre_score = 0
     content.each do |key, value|
-      if answers[key]
+      if choices[key]
         pre_score += 1
       else
         pre_score -= 1
