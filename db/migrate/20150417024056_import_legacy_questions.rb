8745de19da2ccd723795556da62468bf2ed33ae5
Larry Cao <larrycao@berkeley.edu>
2015-04-22 23:33:00 -0700

    don't set id explicitly

--- db/migrate/20150417024056_import_legacy_questions.rb
+++ db/migrate/20150417024056_import_legacy_questions.rb
@@ -3,7 +3,6 @@ class ImportLegacyQuestions < ActiveRecord::Migration
   	ActiveRecord::Base.connection.execute("DELETE from \"questions_v2\"")
   	Question.all.each do |old|
   		q = ShortAnswerQuestion.build :content => old.content, :solution => old.solution.content, :rubric => old.rubric.rubric, :lesson => old.lesson, :difficulty => old.difficulty
-  		q.id = old.id
   		q.save!
   	end
   end
