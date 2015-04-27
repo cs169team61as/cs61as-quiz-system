53066ac03fa59fa005decaf99a72c636672744cc
lenazh <lena.zhivun@berkeley.edu>
2015-04-27 01:00:11 -0300

    Fixed the migrations

--- db/migrate/20150417024056_import_legacy_questions.rb
+++ db/migrate/20150417024056_import_legacy_questions.rb
@@ -1,20 +1,20 @@
 class ImportLegacyQuestions < ActiveRecord::Migration
 # TODO -- actually test this mirgation
-  def change
+  def up
   	Question.includes(:solution, :rubric).each do |old|
   		if old.rubric and old.solution
   			q = ShortAnswerQuestion.build :content => old.content, 
   		                              :lesson => old.lesson, 
   		                              :difficulty => old.difficulty,
-  		                              :solution => old.solution.read_attribute(content),
-  		                              :rubric => old.rubric.read_attribute(rubric) 
+  		                              :solution => old.solution.read_attribute(:content),
+  		                              :rubric => old.rubric.read_attribute(:rubric) 
   			q.save!
 
-  			Grade.update_all {:question_id => q.id}, {:question_id => old.id} 
-  			Rubric.update_all {:question_id => q.id}, {:question_id => old.id}
-  			Solution.update_all {:question_id => q.id}, {:question_id => old.id}
-  			Relationship.update_all {:question_id => q.id}, {:question_id => old.id}
-  			Submission.update_all {:question_id => q.id}, {:question_id => old.id}
+  			Grade.where(:question_id => old.id).update_all(:question_id => q.id)
+  			Rubric.where(:question_id => old.id).update_all(:question_id => q.id)
+  			Solution.where(:question_id => old.id).update_all(:question_id => q.id)
+  			Relationship.where(:question_id => old.id).update_all(:question_id => q.id)
+  			Submission.where(:question_id => old.id).update_all(:question_id => q.id)
   		end
   	end
   end
