f4c92e041fd41d8ea309745b6665f347d49884b6
lenazh <lena.zhivun@berkeley.edu>
2015-05-13 23:08:45 -0300

    Fixed failing test

--- app/forms/new_all_that_applies_question_form.rb
+++ app/forms/new_all_that_applies_question_form.rb
@@ -3,7 +3,7 @@ class NewAllThatAppliesQuestionForm < NewQuestionForm
 
   	def validate_and_save(question_params)
   		return false unless validate(question_params)
-    	AllThatAppliesQuestion.form_2_choices question, question_params
+    	AllThatAppliesQuestion.form_2_choices @model, question_params
     	save_with_params(question_params)
     end
 
