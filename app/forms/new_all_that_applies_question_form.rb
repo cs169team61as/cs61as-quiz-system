299101e007af66a2faca90a73e9bb605dd0bf865
lenazh <lena.zhivun@berkeley.edu>
2015-05-13 23:04:42 -0300

    Edit forms refactored, form_2_choices is now done by the model

--- app/forms/new_all_that_applies_question_form.rb
+++ app/forms/new_all_that_applies_question_form.rb
@@ -1,12 +1,10 @@
 class NewAllThatAppliesQuestionForm < NewQuestionForm 
 	model :all_that_applies_question
 
-
   	def validate_and_save(question_params)
   		return false unless validate(question_params)
     	AllThatAppliesQuestion.form_2_choices question, question_params
     	save_with_params(question_params)
     end
-
 
 end
